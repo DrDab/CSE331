613d9a199464fe8c0d4bc86282b6e1a6ed464682
Victor Du <vdu@cs.washington.edu>
2022-04-08 11:51:31 -0700

    Added RandomHello class

---
+++ hw-setup/src/main/java/setup/RandomHello.java
@@ -0,0 +1,42 @@
+package setup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/** RandomHello selects and prints a random greeting. */
+public class RandomHello
+{
+    private Random randomGenerator;
+    private String[] greetings;
+
+    public RandomHello()
+    {
+        randomGenerator = new Random();
+        greetings = new String[]
+                                {
+                                    "Hello World",
+                                    "Hola Mundo",
+                                    "Bonjour Monde",
+                                    "Hallo Welt",
+                                    "Ciao Mondo"
+                                };
+    }
+
+    /**
+     * Prints a random greeting to the console.
+     *
+     * @param args command-line arguments (ignored)
+     */
+    public static void main(String[] args)
+    {
+        RandomHello randomHello = new RandomHello();
+        System.out.println(randomHello.getGreeting());
+    }
+
+    /** @return a greeting, randomly chosen from five possibilities */
+    public String getGreeting()
+    {
+        return greetings[randomGenerator.nextInt(greetings.length)];
+    }
+}
