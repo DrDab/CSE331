26f2a63f6e247921d9039b5811d414cba3bdd783
Victor Du <vdu@cs.washington.edu>
2022-05-04 21:12:55 -0700

    Moved Edge class tests from TestEdge to TestDirectedGraph

--- hw-graph/src/test/java/graph/junitTests/TestDirectedGraph.java
+++ hw-graph/src/test/java/graph/junitTests/TestDirectedGraph.java
@@ -1,6 +1,7 @@
 package graph.junitTests;
 
 import graph.DirectedGraph;
+import graph.Edge;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -358,5 +359,149 @@ public class TestDirectedGraph
         assertTrue(g2.hasEdge("BRAVO", "BRAVO", "bb"));
         assertTrue(g2.getEdges("BRAVO", "BRAVO").contains("bb"));
         assertTrue(g2.getChildNodes("BRAVO").contains("BRAVO"));
+    }
+
+    // Edge class tests below
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSrcNodeEdgeConstructorThrowsException()
+    {
+        Edge e = new Edge(null, "", "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullDestNodeEdgeConstructorThrowsException()
+    {
+        Edge e = new Edge("", null, "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullLabelEdgeConstructorThrowsException()
+    {
+        Edge e = new Edge("", "", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSrcDestNodeEdgeConstructorThrowsException()
+    {
+        Edge e = new Edge(null, null, "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullDestLabelNodeEdgeConstructorThrowsException()
+    {
+        Edge e = new Edge("", null, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSrcLabelEdgeConstructorThrowsException()
+    {
+        Edge e = new Edge(null, "", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullAllParamsConstructorThrowsException()
+    {
+        Edge e = new Edge(null, null, null);
+    }
+
+    private Edge edge;
+    private Edge edgeDup;
+    private Edge edge2;
+    private Edge edge2Dup;
+
+    @Before
+    public void initializeValidEdges()
+    {
+        edge = new Edge("mySrc", "myDest", "myLabel");
+        edgeDup = new Edge("mySrc", "myDest", "myLabel");
+        edge2 = new Edge("mySrc2", "myDest2", "myLabel2");
+        edge2Dup = new Edge("mySrc2", "myDest2", "myLabel2");
+    }
+
+    @Test
+    public void testGetSourceNode()
+    {
+        assertEquals("mySrc", edge.getSourceNode());
+        assertEquals("mySrc", edgeDup.getSourceNode());
+        assertEquals("mySrc2", edge2.getSourceNode());
+        assertEquals("mySrc2", edge2Dup.getSourceNode());
+    }
+
+    @Test
+    public void testGetDestNode()
+    {
+        assertEquals("myDest", edge.getDestNode());
+        assertEquals("myDest", edgeDup.getDestNode());
+        assertEquals("myDest2", edge2.getDestNode());
+        assertEquals("myDest2", edge2Dup.getDestNode());
+    }
+
+    @Test
+    public void testGetEdgeLabel()
+    {
+        assertEquals("myLabel", edge.getLabel());
+        assertEquals("myLabel", edgeDup.getLabel());
+        assertEquals("myLabel2", edge2.getLabel());
+        assertEquals("myLabel2", edge2Dup.getLabel());
+    }
+
+    @Test
+    public void testEdgeEquals()
+    {
+        // test symmetry of equals
+        assertTrue(edge.equals(edgeDup));
+        assertTrue(edgeDup.equals(edge));
+        // test equals is consistent.
+        assertEquals(edge, edgeDup);
+        Edge nonEqualEdge = new Edge("notMySrc", "notMyDest", "notMyLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+        nonEqualEdge = new Edge("mySrc", "notMyDest", "notMyLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+        nonEqualEdge = new Edge("notMySrc", "myDest", "notMyLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+        nonEqualEdge = new Edge("notMySrc", "notMyDest", "myLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+        nonEqualEdge = new Edge("mySrc", "myDest", "notMyLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+        nonEqualEdge = new Edge("mySrc", "notMyDest", "myLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+        nonEqualEdge = new Edge("notMySrc", "myDest", "myLabel");
+        // test symmetry of equals when false
+        assertFalse(edge.equals(nonEqualEdge));
+        assertFalse(nonEqualEdge.equals(edge));
+    }
+
+    @Test
+    public void testEdgeHashCode()
+    {
+        // test if edge2 == edge2Dup, then edge2.hashCode() == edge2.hashCode()
+        assertEquals(edge, edgeDup);
+        assertEquals(edge.hashCode(), edgeDup.hashCode());
+        // test hashCode is consistent
+        assertEquals(edge.hashCode(), edgeDup.hashCode());
+
+        // test if edge2 == edge2Dup, then edge2.hashCode() == edge2.hashCode()
+        assertEquals(edge2, edge2Dup);
+        assertEquals(edge2.hashCode(), edge2Dup.hashCode());
+        // test hashCode is consistent, again.
+        assertEquals(edge2.hashCode(), edge2Dup.hashCode());
+
+        assertNotEquals(edge.hashCode(), edge2.hashCode());
+        // test if edge.hashCode != edge2.hashCode, then edge != edge2
+        //  (as that is the contrapositive of the implication that:
+        //      if edge == edge2, then edge.hashCode == edge2.hashCode .)
+        assertNotEquals(edge, edge2);
     }
 }
