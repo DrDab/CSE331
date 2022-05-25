a1bbf2671ae1b3f9071c6b5837f2c9144461cb4e
Victor Du <vdu@cs.washington.edu>
2022-05-25 11:39:12 -0700

    Added code to get map coordinates on click and do callback in Map

--- hw-lines/src/Map.tsx
+++ hw-lines/src/Map.tsx
@@ -27,7 +27,14 @@ interface MapProps {
 interface MapState {
   myPoints: Point[],
   pointAddX: string,
-  pointAddY: string
+  pointAddY: string,
+  distState: number,
+  distLastLat: number,
+  distLastLng: number,
+  distP1Lat: number,
+  distP1Lng: number,
+  distP2Lat: number,
+  distP2Lng: number
 }
 
 export interface Point
@@ -48,22 +55,14 @@ const pointIcon = L.icon({
   iconAnchor: [15,32]
 });
 
-// click handler to get lat/lon of clicked position on map
-const LocationFinderDummy = () => {
-  const map = useMapEvents({
-      click(e) {
-          console.log(e.latlng);
-      },
-  });
-  return null;
-};
-
 class Map extends Component<MapProps, MapState> 
 {
+
   constructor(props: any)
   {
     super(props);
-    this.state = { myPoints: [], pointAddX : "", pointAddY : "" };
+    this.state = { myPoints: [], pointAddX: "", pointAddY: "", distState: 0, distP1Lat: 0, 
+                    distP1Lng: 0, distP2Lat: 0, distP2Lng: 0, distLastLat: 0, distLastLng: 0 };
   }
 
   render() 
@@ -72,6 +71,16 @@ class Map extends Component<MapProps, MapState>
     let edges = this.props.myEdges;
     let points = this.state.myPoints;
 
+    const LocationFinderDummy = (props: {onClick(lat:number, lng:number) : void}) => {
+      const map = useMapEvents({
+          click(e) {
+            let latLng = e.latlng;
+            props.onClick(latLng.lat, latLng.lng);
+          },
+      });
+      return null;
+    };
+
     return (
       <div id="map">
         <MapContainer
@@ -79,12 +88,16 @@ class Map extends Component<MapProps, MapState>
           zoom={15}
           scrollWheelZoom={false}
         >
-          <LocationFinderDummy />
 
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+
+          <LocationFinderDummy onClick={(lat, lng) => {
+            console.log("Map coordinates clicked: Lat=" + lat + ", Lng=" + lng);
+            this.setState({distLastLat: lat, distLastLng: lng})
+          }} />
 
           {
             edges.map(function (edge) 
@@ -178,12 +191,22 @@ class Map extends Component<MapProps, MapState>
         <br/>
         <button onClick={() => 
             {
-              
+              let distState = this.state.distState;
+              if (distState == 0)
+              {
+                this.setState({ distState: 1 });
+              }
+              else
+              {
+                this.setState({ distState: 0 });
+              }
             }}>
           Measure Distance
         </button>
         &nbsp;
-        {this.state.myPoints}
+        {this.state.distState}
+        &nbsp;
+        {this.state.distLastLat + "," + this.state.distLastLng}
       </div>
     );
   }
